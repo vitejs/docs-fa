81ecda73a9c567181b8440a585f54f1391c21918
Mostafa <nematpor46@gmail.com>
2024-11-19 11:33:16 +0330

    Initial commit

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,23 @@
+import { h } from 'vue'
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client'
+import '@shikijs/vitepress-twoslash/style.css'
+import './styles/vars.css'
+import './styles/landing.css'
+import AsideSponsors from './components/AsideSponsors.vue'
+import SvgImage from './components/SvgImage.vue'
+import 'virtual:group-icons.css'
+
+export default {
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'aside-ads-before': () => h(AsideSponsors),
+    })
+  },
+  enhanceApp({ app }) {
+    app.component('SvgImage', SvgImage)
+    app.use(TwoslashFloatingVue)
+  },
+} satisfies Theme
